a8dad99b5f284f6454e8eb554b6c722f914c6333
yangyue <yangyueyul@gmail.com>
2020-08-21 20:27:49 +0800

    feat: add mta

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,6 +20,20 @@ module.exports = {
     editLinkText: '在GitHub上编辑此页面',
   },
   smoothScroll: true,
+  head: [
+    ['script',{},`
+        var _mtac = {"senseQuery":1};
+        (function() {
+          var mta = document.createElement("script");
+          mta.src = "//pingjs.qq.com/h5/stats.js?v2.0.4";
+          mta.setAttribute("name", "MTAH5");
+          mta.setAttribute("sid", "500727815");
+          mta.setAttribute("cid", "500727816");
+          var s = document.getElementsByTagName("script")[0];
+          s.parentNode.insertBefore(mta, s);
+        })();
+    `]
+  ]
 };
 
 function getWebSidebar() {
